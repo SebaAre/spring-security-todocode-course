9e7fcc569f81f5eade0fd1757e62fe9d972c1489
SebaAre <arellanosebastian5@hotmail.com>
2025-11-03 11:45:31 -0300

    First commit: Spring Security first project setup

---
+++ educative-platform/src/test/java/com/sebastianarellano/educative_platform/EducativePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastianarellano.educative_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducativePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
